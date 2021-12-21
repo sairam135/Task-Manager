76bdcc1890eb2614c8d2486725af1dec86aa03ba
sairam135 <67577101+sairam135@users.noreply.github.com>
2021-12-21 07:48:42 +0530

    Mongoose URL

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose=require('mongoose')
 let user=require('../models/user')
 let task=require('../models/task')
-let url="mongodb+srv://taskmanager:<taskmanager>@cluster0.nsa1p.mongodb.net/taskmanager?retryWrites=true&w=majority"
+let url="mongodb+srv://taskmanager:taskmanager@cluster0.nsa1p.mongodb.net/taskmanager?retryWrites=true&w=majority"
 mongoose.connect(url,{
     useNewUrlParser:true
 })
