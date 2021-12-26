14ecf6bda69808602dd35639d7757a4716e9b35b
sairam135 <67577101+sairam135@users.noreply.github.com>
2021-12-26 07:28:22 +0530

    production build

--- src/index.js
+++ src/index.js
@@ -9,25 +9,12 @@ let port=process.env.PORT
 
 app.use(express.json())
 app.use(cors());
-app.use('',(req,res,next)=>{
-    console.log(req.url)
-    if(!req.url.match(/^\/api/)){
-        app.use(express.static(path.join(__dirname, '../build')));
-        app.get('/', function (req, res) {
-          res.sendFile(path.join(__dirname, '../build/index.html'));
-        });
-    }
-    else{
-        next()
-    }
-})
-
-
-app.get('/home',(req,res)=>{
-    res.send('Home')
-})
+app.use(express.static(path.join(__dirname, '../build')));
 app.use(userRouter)
 app.use(taskRouter)
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 app.listen(port,()=>{
     console.log("Server started")
 }) 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,7 @@
 let jwt=require('jsonwebtoken')
 let user=require('../models/user')
 let auth=async(req,res,next)=>{
-    //console.log(req.body)
     try {
-        console.log("Author",req.header('Authorization'))
         let token=req.header('Authorization').replace('Bearer ','')
         let decoded=jwt.verify(token,process.env.JWT_SECRET)
         let User=await user.findOne({_id:decoded._id,'tokens.token':token})
@@ -14,7 +12,6 @@ let auth=async(req,res,next)=>{
         req.User=User
         next()
     } catch (error) {
-        console.log("error",error)
         res.status(401).send({error:"Please Authenticate"})
     }
 }
